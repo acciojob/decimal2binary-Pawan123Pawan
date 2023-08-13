3f06140b40f129bb1db7cba2db04b65ad657a67a
Pawan123Pawan <105289150+Pawan123Pawan@users.noreply.github.com>
2023-08-13 17:36:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function decimalToBinary(num) {
-  //Write you code here
-  
+    let s = "";
+    while (num > 0) {
+        if (num % 2 !== 0) s += "1";
+        else s += "0";
+         num = Math.floor(num / 2);
+    }
+
+    return s.split("").reverse().join(""); 
 }
 
 window.decimalToBinary = decimalToBinary;
